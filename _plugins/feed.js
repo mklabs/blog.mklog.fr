7beb9c47f105de909cd5e196eb5a3b3d7cc3bdc9
Mickael Daniel <daniel.mickael@gmail.com>
2012-03-14 13:53:16 +0100

    atom feed
    with custom generator, done as a silly workaround to feed spam. Only posts
    published after 03-01-2012

---
+++ _plugins/feed.js
@@ -0,0 +1,26 @@
+
+
+var nabe = require('../../nabe'),
+  moment = nabe.moment;
+
+
+//
+// Custom feed generator, just for the sake of controlling from which date
+// the feed should begin. A silly workaround to feed spam when re-generating content.
+//
+
+var Feed = module.exports = nabe.Generator.extend({
+
+  from: moment('03-01-2012', 'DD-MM-YYYY').valueOf(),
+
+  generate: function generate(site) {
+    nabe.log.info('feed:generator', 'Filtering posts for feed from ', moment(this.from).format('LL'));
+    var from = this.from;
+
+    var posts = site.posts.filter(function(post) {
+      return from < post.get('time');
+    });
+
+    site.set('feed_posts', posts);
+  }
+});
